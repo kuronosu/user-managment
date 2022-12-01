fa952082dab8fa42a0032512725fd55f6c3b322f
David Alejandro Espinosa <davidespinosa4635@gmail.com>
2022-12-01 16:55:14 -0500

    Form and buttons added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Fragment, useState} from 'react';
 
 function App() {
+
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefono, setTelefono] = useState(0);
+
+  function Agregar(){
+    console.log('nombre: ', nombre)
+  }
+  
+  function Modificar(){
+    console.log('Modificar')
+  }
+  
+  function Eliminar(){
+    console.log('Eliminar')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div>
+        <label>Nombre</label>
+        <input type='text' placeholder="Nombre" onChange={(e) => {setNombre(e.target.value)}}/>
+        <label>Email</label>
+        <input type='text' placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} />
+        <label>Telefono</label>
+        <input type='number' placeholder="Telefono" onChange={(e) => {setTelefono(e.target.value)}} />
+      </div>
+      <div>
+        <button onClick={Agregar} >Agregar</button>
+        <button onClick={Modificar} >Modificar</button>
+        <button onClick={Eliminar} >Eliminar</button>
+      </div>
+    </Fragment>
   );
 }
 
